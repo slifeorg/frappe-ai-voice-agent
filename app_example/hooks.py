a54b40accb948c095bce9ce30d25862d17ea05a9
Andrew Pavlichuk <133572273+andriipavlichuk@users.noreply.github.com>
2025-06-24 11:30:11 +0000

    Added a Vue component

--- app_example/hooks.py
+++ app_example/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/app_example/css/app_example.css"
-# app_include_js = "/assets/app_example/js/app_example.js"
+app_include_js = "https://unpkg.com/vue@3"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/app_example/css/app_example.css"
